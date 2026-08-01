4e15f6d1ceda936baf0e193472da72843ac55560
alirobatic <alirezaaskary23@gmail.com>
2026-08-01 13:10:18 +0330

    Add files via upload

---
+++ tamrin 1 git hub.py
@@ -0,0 +1,50 @@
+print("Hello word") 
+
+# add
+a = 20
+z = 10
+print(a+z)
+print(a-z)
+print(a/z)
+print(a*z)
+
+print("/--------------------------")
+print(a//z)
+print(a%z)
+print(a**z)
+
+print("/--------------------------")
+ 
+n = int(input("number1="))
+q = int(input("number2="))
+print(n+q)
+print(n/q)
+print(n-q)
+print(n*q)
+
+
+y = input("name and family name")
+print(y)
+
+
+a = int(input("num1="))
+b = int(input("number2="))
+print(a+b)
+# jame to adad 
+
+
+
+
+        
+        
+
+
+
+
+
+
+9
+
+
+
+
