d6c496b4fe502ca9c31fdb617b85a0a0ae18a1fb
alirobatic <alirezaaskary23@gmail.com>
2026-08-02 22:04:32 +0330

    Add files via upload

---
+++ practice 2.py
@@ -0,0 +1,40 @@
+a = "20"
+print(type(a))
+
+a = input("name =")
+print(type(a))
+
+a = float(input("num"))
+print(type(a))
+
+a = input("name =")
+b = input("family=")
+print(a+ " " +b)
+
+print(("ali" + " ") * 3)
+
+a = input("name=")
+print(len(a))
+
+a = input("name=")
+print(a[1:4])
+
+a = "0123456"
+print(a[1:4])
+
+b ="12345678"
+print(len(b))
+
+m = "0123456789"
+print(m[1:6:2])
+
+a = "alireza"
+print(a.capitalize())
+
+b = input("name=")
+print(b.capitalize())
+
+
+
+
+
