36a8a619fd4e22a4f5206b096524f7d76b1cb215
alireza <alirezaaskary23@gmail.com>
2026-08-20 14:20:49 +0330

    Add files via upload

---
+++ bool.py
@@ -0,0 +1,51 @@
+b = True
+print(type(b))
+
+print(".............")
+a = "Ali"
+print(a=="ali")
+
+print(".............")
+
+my_dic = {"solo":10 , "azot":20 , "fosfor":15 , "20_20_20":40}
+print(my_dic =="azot")
+
+print(".............")
+
+my_list = ["ali" , "reza " , "saleh"]
+print(my_list == "ali")
+
+print(".............")
+
+#name = input("name = ")
+#age = int(input("age = "))
+#city = input("city")
+#print(name == "ali" and age == 27)
+
+#print(name=="ali" or name == "reza")
+
+print(".............")
+
+e = "12"
+print(e.isdigit())
+e1 = "1.2"
+print(e1.isdigit())
+
+print("..............")
+
+print(bool({}))
+print(bool({12}))
+
+print("..............")
+
+#num = int(input("number = "))
+#print(num%2==0)
+
+print("..............")
+
+stu = True
+print(not stu)
+
+
+
+
